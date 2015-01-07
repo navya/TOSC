bcda56e4fa9b6681a6f6ce6a78c86711c4e21070
bhanuc <bhanuc@iitk.ac.in>
2015-01-07 13:14:38 +0530

    fixed the tests

--- api/test.js
+++ api/test.js
@@ -3,21 +3,19 @@ var request = require('supertest');
 var api = require('../index.js');
 
 
-describe('GET /single', function(){
-  it('should respond with a single result', function(done){
+describe('GET /result', function(){
+     //this.timeout(15000);
+  it('should respond with a single result Page', function(done){
     var app = api;
 
     request(app.listen())
-    .get('/result')
-    .query({ rollno: 'new' })
+    .get('/result?rollno=0000000')
     .set('Accept-Encoding', 'gzip')
     .expect(200)
-    .expect('Content-Type', /json/)
+    .expect('Content-Type', 'text/html; charset=utf-8')
     .end(function(err, res){
     if (err) throw err;
     else {
-        if (!('_id' in res.body)) return "missing id";
-    if (!('rollno' in res.body)) throw new Error("missing rollno");
         done();
     }
   });
@@ -29,16 +27,13 @@ describe('GET /phone', function(){
     var app = api;
 
     request(app.listen())
-    .get('/phone')
-    .query({ phone: '00000000' }) // No actual no.s are written here for privacy reasons
+    .get('/phone?phone=0000000') // No actual no.s are written here for privacy reasons
     .set('Accept-Encoding', 'gzip')
     .expect(200)
-    .expect('Content-Type', /json/)
+    .expect('Content-Type', 'text/html; charset=utf-8')
     .end(function(err, res){
     if (err) throw err;
     else {
-        if (!('_id' in res.body)) return "missing id";
-    if (!('phone' in res.body)) throw new Error("missing phone");
         done();
     }
   });
